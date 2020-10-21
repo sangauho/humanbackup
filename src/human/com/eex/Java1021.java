a69d45a4b7b96f943bca3d37b93501cda4cf3688
USER <USER@USER-PC.local>
2020-10-21 11:07:55 +0900

    ㅁㄴㅇㄴㅇ

---
+++ src/human/com/eex/Java1021.java
@@ -0,0 +1,11 @@
+package human.com.eex;
+
+public class Java1021 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Å×½ºÆ®");
+	}
+
+}
---
+++ src/human/com/eex/JavaClass1021.java
@@ -0,0 +1,12 @@
+package human.com.eex;
+
+public class JavaClass1021 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Å×½ºÆ®");
+
+	}
+
+}
